18dda2b76f392677c9a8088cae70b576bec7505a
erikk03 <sdi2100043@di.uoa.gr>
2024-08-02 16:44:13 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns :[
+            {
+                protocol: 'https',
+                hostname: 'linkedinsdi2100043.blob.core.windows.net',
+            }
+        ]
+    },
+};
 
 export default nextConfig;
