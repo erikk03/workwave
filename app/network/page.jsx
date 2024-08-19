4230c26729c22f5f3036088805e4a3e3a2f39a0c
erikk03 <sdi2100043@di.uoa.gr>
2024-08-19 22:40:49 +0300

    style header icons

--- app/network/page.jsx
+++ app/network/page.jsx
@@ -1,5 +1,17 @@
 import Network from "@/components/Network";
+import Header from "@/components/Header";
 
 export default function NetworkPage() {
-    return <Network />;
+
+
+    return (
+    
+    <div className="min-h-screen flex flex-col">
+        <header className="border-b sticky top-0 bg-white z-50">
+            <Header />
+        </header>
+        
+        <Network />
+    </div>
+    );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,25 @@
-"use client"
+"use client";
 
 import { Briefcase, HomeIcon, MessageSquare, SearchIcon, UsersIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
-import { useState } from 'react';
-import {Avatar} from "@nextui-org/avatar";
-import {Button} from "@nextui-org/react";
+import { useState, useEffect } from 'react';
+import { Avatar } from "@nextui-org/avatar";
+import { Button } from "@nextui-org/react";
 import { signOut } from "next-auth/react";
+import { useRouter } from 'next/navigation';
+import {cn} from "@/lib/utils";
 
 export default function Header() {
     const { data: session, status } = useSession();
     const [dropdownVisible, setDropdownVisible] = useState(false);
+    const router = useRouter();
+
+    const [path, setPath] = useState('');
+    useEffect(() => {
+        setPath(window.location.pathname);
+    }, [router]);
 
     const toggleDropdown = () => {
         setDropdownVisible(!dropdownVisible);
@@ -26,74 +34,75 @@ export default function Header() {
     }
 
     return (
-        <div className="flex items-center p-2 mx-auto">
-            <Image
-                className="rounded-lg"
-                src="/favicon.ico"
-                width={40}
-                height={40}
-                alt="logo"
-            />
+        <div className="flex items-center justify-between px-2 py-1 bg-white shadow-md shadow-gray-400 ">
+            <div className="flex items-center spaxe-x-2">
+                <Image
+                    className="rounded-md"
+                    src="/favicon.ico"
+                    width={40}
+                    height={40}
+                    alt="logo"
+                />
 
-            <div className="flex-1">
-                <form className="flex items-center bg-gray-200 rounded-md mx-2 max-w-96">
+                {/* <form className="flex-1 flex items-center bg-gray-200 rounded-md ml-2 px-2 max-w-10">
                     <SearchIcon className="h-4 text-gray-500"/>
                     <input 
                         type="text"
                         placeholder="Search"
-                        className="bg-transparent flex-1 outline-none"
+                        className="bg-transparent flex-1 outline-none text-sm"
                     />
-                </form>
+                </form> */}
             </div>
-        
-            <div className="flex items-center space-x-3 px-3">
-                <Link href="/feed" className="icon">
-                    <HomeIcon className="h-5" />
+            
+            <div className="flex-1 flex items-center space-x-2 justify-center">
+                <Link href="/feed" title="Home Page" >
+                    <HomeIcon className={cn("h-5", path === '/feed' && "text-black fill-blue-500 ")} />
                 </Link>
 
-                <Link href="/network" className="icon hidden md:flex">
-                    <UsersIcon className="h-5" />
+                <Link href="/network" title="Network">
+                    <UsersIcon className={cn("h-5", path === '/network' && "text-black fill-blue-500")} />
                 </Link>
 
-                <Link href="" className="icon hidden md:flex">
-                    <Briefcase className="h-5" />
+                <Link href="/jobs" title="Jobs">
+                    <Briefcase className={cn("h-5", path === '/jobs' && "text-black fill-blue-500")} />
                 </Link>
 
-                <Link href="" className="icon hidden md:flex">
-                    <MessageSquare className="h-5" />
+                <Link href="/communication" title="Communication">
+                    <MessageSquare className={cn("h-5", path === '/communication' && "text-black fill-blue-500")} />
                 </Link>
+            </div>
 
-                <div className="relative">
-                    
-                    <div onClick={toggleDropdown} className="cursor-pointer">
-                    {session?.user?.profileImage ? (
-                        <Avatar
-                            isBordered
-                            color=""
-                            size="sm"
-                            radius="full"
-                            src={session?.user?.profileImage}
-                        />
-                    ):(
-                        <Avatar
-                            isBordered
-                            color=""
-                            size="sm"
-                            radius="full"
-                            name={session?.user?.firstName.charAt(0) + session?.user?.lastName.charAt(0)}
-                        />
-                    )}
-                    </div>
+            <div className="flex relative">
+                
+                <div onClick={toggleDropdown} className="cursor-pointer">
+                {session?.user?.profileImage ? (
+                    <Avatar
+                        isBordered
+                        color=""
+                        size="sm"
+                        radius="full"
+                        src={session?.user?.profileImage}
+                    />
+                ):(
+                    <Avatar
+                        isBordered
+                        color=""
+                        size="sm"
+                        radius="full"
+                        name={session?.user?.firstName.charAt(0) + session?.user?.lastName.charAt(0)}
+                    />
+                )}
+                </div>
 
-                    {dropdownVisible && (
-                        <div className="absolute right-1 mt-2 w-40 bg-white border border-gray-300 rounded shadow-xl">
-                            <Link href="/userinfo" legacyBehavior>
-                                <a className="block px-4 py-2 text-gray-800 hover:bg-gray-300">Profile Information</a>
-                            </Link>
-                            <Link href=""legacyBehavior>
-                                <a className="block px-4 py-2 text-gray-800 hover:bg-gray-300">Profile Settings</a>
-                            </Link>
-                            <a className="flex px-4 py-2 justify-center">
+                {dropdownVisible && (
+                    <div className="absolute right-2 mt-7 w-40 bg-white border border-gray-300 rounded shadow-xl">
+                        <Link href="/userinfo" legacyBehavior>
+                            <a className="block px-4 py-2 text-gray-800 hover:bg-gray-300">Profile Information</a>
+                        </Link>
+                        <Link href=""legacyBehavior>
+                            <a className="block px-4 py-2 text-gray-800 hover:bg-gray-300">Profile Settings</a>
+                        </Link>
+                        <a className="flex px-4 py-2 justify-center">
                             <Button
                                 onClick={() => signOut()}
                                 color="danger"
@@ -104,11 +113,10 @@ export default function Header() {
                             >
                                 Log Out
                             </Button>
-                            </a>
-                        </div>
-                    )}
+                        </a>
+                    </div>
+                )}
 
-                </div>
             </div>
         </div>
     );
