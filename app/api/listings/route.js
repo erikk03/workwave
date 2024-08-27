7c7d792b5add1120a6e7da91e24303b4acd095ec
vaggeliskg <sdi2100045@di.uoa.gr>
2024-08-27 20:04:55 +0300

    jobs based on skills

--- app/api/listings/route.js
+++ app/api/listings/route.js
@@ -6,33 +6,6 @@ import { authOptions } from "../auth/[...nextauth]/route";
 import { NextResponse } from 'next/server';
 import User from "@/models/user"; 
 
-// Fetch all listings
-// export async function GET(request) {
-//     try {
-//         // Get the user session
-//         const session = await getServerSession(authOptions);
-//         if (!session || !session.user?.userId) {
-//             return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-//         }
-
-//         // Connect to the MongoDB database
-//         await connectMongoDB();
-        
-//         // Find all listings and populate related fields
-//         const listings = await Listing.find()
-//             .populate({
-//                 path: 'applicants',
-//                 select: 'applicantId', // Adjust the fields based on your model
-//             })
-//             .lean();
-
-//         return NextResponse.json(listings);
-//     } catch (error) {
-//         console.error("Error fetching listings:", error);
-//         return new NextResponse("Internal server error", { status: 500 });
-//     }
-// }
-
 // Create a new listing
 export async function POST(req) {
     try {
